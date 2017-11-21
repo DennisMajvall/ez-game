121bc5d991a7b49cd1f645f87fd4f3a8d1fc6720
HMikhaeljan <Hovsep_@hotmail.com>
2017-11-21 10:09:40 +0100

    Added stuff to cow

--- modules/monster.js
+++ modules/monster.js
@@ -6,7 +6,9 @@ const gamefieldWidth = 9800;
 module.exports = class Monster {
     constructor(type, monsterId){
     this.monsterId = monsterId;
-    this.type = type; 
+    this.type = type;
+    this.counter = 0;
+    this.lastDirection = 90; 
     Object.assign(this, global.json.monsters.default, global.json.monsters[type]);
     this.x = Math.round(Math.random() * gamefieldWidth + 100);
     this.y = Math.round(Math.random() * gamefieldHeight + 100);
@@ -20,8 +22,23 @@ module.exports = class Monster {
 
   updateMovement(){
     if(this.alive){
-      this.y -=1;
-    
+      if(this.type == 'cow'){
+        if(this.lastDirection == 90){
+          this.y -=1;
+          this.counter++;
+          if(this.counter >= 100){
+            this.lastDirection = 180; 
+            this.counter = 0;
+          }
+        }else if(this.lastDirection == 180){
+            this.y +=1;
+            this.counter++;
+            if(this.counter >= 100){
+              this.lastDirection = 90; 
+              this.counter = 0;
+            }
+        }
+      }
     }
   }
 }
